9932170fd511114574a721c1306dcf1f72fccbe1
Arnaud Taffanel <unknown>
2013-06-12 00:46:59 +0200

    Flasher GUI: Replaced stdout/print hack by logging handlers

--- usbtools/cradioFlasher.py
+++ usbtools/cradioFlasher.py
@@ -9,6 +9,10 @@ import os
 import json
 import hashlib
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 # Page0 to be written first to make the flash process safer
 page0 = (0x02, 0x78, 0x00)  #LJMP 0x7800  ; Jump to the bootloader
 
@@ -20,6 +24,10 @@ class CradioFlasher(QtGui.QMainWindow):
         self.browseButton.clicked.connect(self.browse)
         self.flashButton.clicked.connect(self.flash)
         self.fileLine.returnPressed.connect(self.file_selected)
+        
+		# Log messages are printed in the GUI textbox
+        logger.addHandler(logging.StreamHandler(self))
+        logger.setLevel(logging.INFO)
         
         self.image_file = None
         self.image_ok = False        # Image file exists
@@ -71,7 +79,7 @@ class CradioFlasher(QtGui.QMainWindow):
             hcri.update(cri.read("manifest.json"));
             
             if hcri.hexdigest() != cri.read("hash").split(':')[1].strip():
-                print("Damaged CRI image!")
+                logger.error("Damaged CRI image!")
                 self.image_ok = False
                 self.image_checked = False
                 self.image_verified = False
@@ -79,27 +87,27 @@ class CradioFlasher(QtGui.QMainWindow):
                 self.imageIdLine.setText("<div style='color:red'>None</div>")
                 return
             
-            print("Loaded image size {} bytes".format(len(image)))
+            logger.info("Loaded image size {} bytes".format(len(image)))
             if manifest["tag"] != "" and manifest["tag"] != "tip":
-                print("Firmware version: {}".format(manifest['tag']))
+                logger.info("Firmware version: {}".format(manifest['tag']))
                 version = manifest['tag']
             else:
                 version = manifest['version']
-            print("Firmware commit: {}".format(manifest['version']))
-            print("Verified image: {}".format(manifest['verified']))
+            logger.info("Firmware commit: {}".format(manifest['version']))
+            logger.info("Verified image: {}".format(manifest['verified']))
             
             self.imageIdLine.setText("Crazyradio {}".format(version))
             self.image_ok = True
             self.image_checked = True
             self.image_verified = manifest['verified']
             self.image = image
-            print("{} Loaded ".format(os.path.basename(self.image_file)))
+            logger.info("{} Loaded ".format(os.path.basename(self.image_file)))
         except Exception as e:
             self.image_ok = False
             self.image_checked = False
             self.image_verified = False
             self.image = None
-            print("Error while checking the image: {}".format(e))
+            logger.error("Error while checking the image: {}".format(e))
     
     def analyze_binary(self):
         self.image_ok = False
@@ -111,7 +119,7 @@ class CradioFlasher(QtGui.QMainWindow):
             with open(self.image_file) as bfile:
                 data = bfile.read()
             
-            print("Image size: {}".format(len(data)))
+            logger.info("Image size: {}".format(len(data)))
             
             if bytearray(data)[0] != 0x02:  #MSC51 jump
                 raise Exception("This does not look like a 8051 image, should start by 0x02")
@@ -119,13 +127,13 @@ class CradioFlasher(QtGui.QMainWindow):
             self.image = data
             self.image_ok = True
             self.imageIdLine.setText("8051 binary image")
-            print("Loaded binary image.")
+            logger.info("Loaded binary image.")
         except Exception as e:
-            print("Error when opening binary image file: {}".format(e))
+            logger.error("Error when opening binary image file: {}".format(e))
 
     def flash(self):
         if not self.image_ok:
-            print("Image not verified, not flashing!")
+            logger.error("Image not verified, not flashing!")
             return
         
         if not self.image_checked or not self.image_verified:
@@ -135,36 +143,36 @@ class CradioFlasher(QtGui.QMainWindow):
                       "reflash the radio.\nDo you want to flash anyway?",
                       QtGui.QMessageBox.No, QtGui.QMessageBox.Yes)
             if reply == QtGui.QMessageBox.No:
-                print("Aborting flash operation.")
+                logger.error("Aborting flash operation.")
                 return
         
         bootloader = False
         try:
             self.setEnabled(False)
             
-            print("")
-            print("Starting to flash...")
+            logger.info("")
+            logger.info("Starting to flash...")
             
             ret = launchBootloader.launchBootloader(True)
             if ret:
-                print("Error!, cannot start the bootloader!")
+                logger.error("Error!, cannot start the bootloader!")
                 raise Exception()
             
             bootloader = NrfUsbBootloader.Bootloader()
             if not bootloader.open():
-                print("Error! Cannot open bootloader.")
+                logger.error("Error! Cannot open bootloader.")
                 raise Exception()
             
-            print("Bootloader version {}".format(bootloader.getVersion()))
+            logger.info("Bootloader version {}".format(bootloader.getVersion()))
             
-            print("Flashing safe page0 (goto bootloader)...")
+            logger.info("Flashing safe page0 (goto bootloader)...")
             bootloader.write(page0, 0)
             
-            print("Flashing new firmware but page 0...")
+            logger.info("Flashing new firmware but page 0...")
             bootloader.write(self.image[NrfUsbBootloader.PAGE_SIZE:],
                              NrfUsbBootloader.PAGE_SIZE)
             
-            print("Verifying new firmware (but page 0)...")
+            logger.info("Verifying new firmware (but page 0)...")
             readback = bootloader.read(NrfUsbBootloader.PAGE_SIZE,
                         len(self.image)-NrfUsbBootloader.PAGE_SIZE)
             for i in range(len(self.image)-NrfUsbBootloader.PAGE_SIZE):
@@ -172,21 +180,21 @@ class CradioFlasher(QtGui.QMainWindow):
                     raise Exception("Verification failed at position {}! {}!={}".format(
                             i, bytearray(self.image)[i+NrfUsbBootloader.PAGE_SIZE], readback[i]))
 
-            print("{} bytes verified!".format(len(self.image)-NrfUsbBootloader.PAGE_SIZE))
+            logger.info("{} bytes verified!".format(len(self.image)-NrfUsbBootloader.PAGE_SIZE))
             
-            print("Flashing page0...")
+            logger.info("Flashing page0...")
             bootloader.write(self.image[:NrfUsbBootloader.PAGE_SIZE], 0)
             
-            print("Verifying full firmware...")
+            logger.info("Verifying full firmware...")
             readback = bootloader.read(0, len(self.image))
             for i in range(len(self.image)):
                 if bytearray(self.image)[i] != readback[i]:
                     raise Exception("Verification failed at position {}! {}!={}".format(
                             i, bytearray(self.image)[i], readback[i]))
             
-            print("{} bytes verified!".format(len(self.image)))
+            logger.info("{} bytes verified!".format(len(self.image)))
             
-            print("Done! You can remove and re-insert the dongle to start"
+            logger.info("Done! You can remove and re-insert the dongle to start"
                   " the new firmware.")
             
         finally:
@@ -201,8 +209,7 @@ class CradioFlasher(QtGui.QMainWindow):
             	version = dev.deviceVersion
             else:
             	version = "{:03x}".format(dev.deviceVersion)
-            	version = "Crazyradio dongle v{}.{}".format(
-            	                     version[:-2], version[-2:])
+            	version = "{}.{}".format(version[:-2], version[-2:])
             self.deviceLine.setText("Crazyradio dongle v{}".format(version))
             self.device_ok = True
         elif launchBootloader.findDevice(0x1915, 0x0101):
@@ -216,12 +223,10 @@ class CradioFlasher(QtGui.QMainWindow):
         self.flashButton.setEnabled(self.device_ok and self.image_ok)
     
     def write(self, msg):
-        """Function called for stdout text"""
+        """Function called for log output"""
         self.logText.setPlainText(self.logText.toPlainText()+msg)
         sb = self.logText.verticalScrollBar()
         sb.setValue(sb.maximum())
-        stdout.write(msg)
-        stdout.flush()
         app.processEvents() #Make sure the GUI is updated during long operations
     
     def flush(self):
@@ -229,11 +234,12 @@ class CradioFlasher(QtGui.QMainWindow):
 
 if __name__ == "__main__":
     import sys
+    
+    # When launched in standalone, also output log in stdout
+    logger.addHandler(logging.StreamHandler(sys.stdout))
+    
     app = QtGui.QApplication(sys.argv)
     ui = CradioFlasher()
-    stdout = sys.stdout
-    sys.stdout = ui
-    sys.stderr = ui
     ui.show()
     sys.exit(app.exec_())
 
