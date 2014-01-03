24c78296f1bbd2ac0dd582ce4487ea88f46b4ff6
Arnaud Taffanel <unknown>
2014-01-03 10:06:18 +0100

    Sync crazyradio python driver with Crazyflie client

--- lib/crazyradio.py
+++ lib/crazyradio.py
@@ -1,31 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
-#    ||          ____  _ __
-# +------+      / __ )(_) /_______________ _____  ___
-# | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
-# +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
-#  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+#     ||          ____  _ __
+#  +------+      / __ )(_) /_______________ _____  ___
+#  | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+#  +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+#   ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
 #
+#  Copyright (C) 2011-2013 Bitcraze AB
 #
-# Copyright (C) 2011-2013 Bitcraze AB
+#  Crazyflie Nano Quadcopter Client
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA  02110-1301, USA.
 
 """
 USB driver for the Crazyradio USB dongle.
@@ -34,8 +33,11 @@ USB driver for the Crazyradio USB dongle.
 __author__ = 'Bitcraze AB'
 __all__ = ['Crazyradio']
 
+
 import os
 import usb
+import logging
+logger = logging.getLogger(__name__)
 
 #USB parameters
 CRADIO_VID = 0x1915
@@ -45,13 +47,13 @@ CRADIO_PID = 0x7777
 #See http://wiki.bitcraze.se/projects:crazyradio:protocol for documentation
 SET_RADIO_CHANNEL = 0x01
 SET_RADIO_ADDRESS = 0x02
-SET_DATA_RATE     = 0x03
-SET_RADIO_POWER   = 0x04
-SET_RADIO_ARD     = 0x05
-SET_RADIO_ARC     = 0x06
-ACK_ENABLE        = 0x10
-SET_CONT_CARRIER  = 0x20
-SCANN_CHANNELS    = 0x21
+SET_DATA_RATE = 0x03
+SET_RADIO_POWER = 0x04
+SET_RADIO_ARD = 0x05
+SET_RADIO_ARC = 0x06
+ACK_ENABLE = 0x10
+SET_CONT_CARRIER = 0x20
+SCANN_CHANNELS = 0x21
 LAUNCH_BOOTLOADER = 0xFF
 
 try:
@@ -60,60 +62,62 @@ try:
 except:
     pyusb1 = False
 
-def findDevices():
+
+def _find_devices():
     """
     Returns a list of CrazyRadio devices currently connected to the computer
     """
     ret = []
 
     if pyusb1:
-        #FIXME: pyUSB 1.x should also be able to return a list of device!
-        dev = usb.core.find(idVendor=0x1915, idProduct=0x7777)
-        if dev!=None:
-            ret += [dev,]
+        dev = usb.core.find(idVendor=0x1915, idProduct=0x7777, find_all=1)
+        if dev is not None:
+            ret = dev
     else:
-      busses = usb.busses()
-      for bus in busses:
-        for device in bus.devices:
-          if device.idVendor == CRADIO_VID:
-            if device.idProduct == CRADIO_PID:
-              ret += [device, ]
+        busses = usb.busses()
+        for bus in busses:
+            for device in bus.devices:
+                if device.idVendor == CRADIO_VID:
+                    if device.idProduct == CRADIO_PID:
+                        ret += [device, ]
 
     return ret
 
-class radioAck:
+
+class _radio_ack:
     ack = False
     powerDet = False
     retry = 0
     data = ()
 
+
 class Crazyradio:
     """ Used for communication with the Crazyradio USB dongle """
     #configuration constants
     DR_250KPS = 0
-    DR_1MPS   = 1
-    DR_2MPS   = 2
+    DR_1MPS = 1
+    DR_2MPS = 2
 
     P_M18DBM = 0
     P_M12DBM = 1
-    P_M6DBM  = 2
-    P_0DBM   = 3
+    P_M6DBM = 2
+    P_0DBM = 3
 
-
-    def __init__(self, device=None):
-	""" Create object and scan for USB dongle if no device is supplied """
-        if device == None:
+    def __init__(self, device=None, devid=0):
+        """ Create object and scan for USB dongle if no device is supplied """
+        if device is None:
             try:
-                device = findDevices()[0]
+                device = _find_devices()[devid]
             except Exception:
                 raise Exception("Cannot find a Crazyradio Dongle")
 
         self.dev = device
 
-        if (pyusb1 == True):
+        if (pyusb1 is True):
             self.dev.set_configuration(1)
             self.handle = self.dev
-            self.version = float( "{0:x}.{1:x}".format(self.dev.bcdDevice>>8, self.dev.bcdDevice&0x0FF) )
+            self.version = float("{0:x}.{1:x}".format(self.dev.bcdDevice >> 8,
+                                 self.dev.bcdDevice & 0x0FF))
         else:
             self.handle = self.dev.open()
             self.handle.setConfiguration(1)
@@ -124,24 +128,24 @@ class Crazyradio:
             raise "This driver requires Crazyradio firmware V0.3+"
 
         if self.version < 0.4:
-            print "Warning: you should update to Crazyradio firmware V0.4+"
+            logger.warning("You should update to Crazyradio firmware V0.4+")
 
         #Reset the dongle to power up settings
-        self.setDatarate(self.DR_2MPS)
-        self.setChannel(2)
+        self.set_data_rate(self.DR_2MPS)
+        self.set_channel(2)
         self.arc = -1
         if self.version >= 0.4:
-            self.setContCarrier(False)
-            self.setAddress((0xE7,)*5)
-            self.setPower(self.P_0DBM)
-            self.setArc(3)
-            self.setArdBytes(32)
+            self.set_cont_carrier(False)
+            self.set_address((0xE7,) * 5)
+            self.set_power(self.P_0DBM)
+            self.set_arc(3)
+            self.set_ard_bytes(32)
 
     def close(self):
-        if (pyusb1 == False):
+        if (pyusb1 is False):
             if self.handle:
-                self.handle.releaseInterface();
-                self.handle.reset();
+                self.handle.releaseInterface()
+                self.handle.reset()
         else:
             if self.dev:
                 if os.name != 'nt':
@@ -150,85 +154,101 @@ class Crazyradio:
         self.handle = None
         self.dev = None
 
-
     ### Dongle configuration ###
-    def setChannel(self, channel):
+    def set_channel(self, channel):
         """ Set the radio channel to be used """
-        sendVendorSetup(self.handle, SET_RADIO_CHANNEL, channel, 0, ())
+        _send_vendor_setup(self.handle, SET_RADIO_CHANNEL, channel, 0, ())
 
-    def setAddress(self, address):
+    def set_address(self, address):
         """ Set the radio address to be used"""
         if len(address) != 5:
-            raise Exception("Crazyradio: the radio address shall be 5 bytes long")
+            raise Exception("Crazyradio: the radio address shall be 5"
+                            " bytes long")
 
-        sendVendorSetup(self.handle, SET_RADIO_ADDRESS, 0, 0, address)
+        _send_vendor_setup(self.handle, SET_RADIO_ADDRESS, 0, 0, address)
 
-    def setDatarate(self, datarate):
+    def set_data_rate(self, datarate):
         """ Set the radio datarate to be used """
-        sendVendorSetup(self.handle, SET_DATA_RATE, datarate, 0, ())
+        _send_vendor_setup(self.handle, SET_DATA_RATE, datarate, 0, ())
 
-    def setPower(self, power):
+    def set_power(self, power):
         """ Set the radio power to be used """
-        sendVendorSetup(self.handle, SET_RADIO_POWER, power, 0, ())
+        _send_vendor_setup(self.handle, SET_RADIO_POWER, power, 0, ())
 
-    def setArc(self, arc):
+    def set_arc(self, arc):
         """ Set the ACK retry count for radio communication """
-        sendVendorSetup(self.handle, SET_RADIO_ARC, arc, 0, ())
+        _send_vendor_setup(self.handle, SET_RADIO_ARC, arc, 0, ())
         self.arc = arc
 
-    def setArdTime(self, ns):
+    def set_ard_time(self, us):
         """ Set the ACK retry delay for radio communication """
-        sendVendorSetup(self.handle, SET_RADIO_ARD, int((us/250)+1), 0, ())
+        # Auto Retransmit Delay:
+        # 0000 - Wait 250uS
+        # 0001 - Wait 500uS
+        # 0010 - Wait 750uS
+        # ........
+        # 1111 - Wait 4000uS
 
-    def setArdBytes(self, nbytes):
-        sendVendorSetup(self.handle, SET_RADIO_ARD, 0x80 | nbytes, 0, ())
+        # Round down, to value representing a multiple of 250uS
+        t = int((us / 250) - 1)
+        if (t < 0):
+            t = 0
+        if (t > 0xF):
+            t = 0xF
+        _send_vendor_setup(self.handle, SET_RADIO_ARD, t, 0, ())
 
-    def setContCarrier(self, active):
+    def set_ard_bytes(self, nbytes):
+        _send_vendor_setup(self.handle, SET_RADIO_ARD, 0x80 | nbytes, 0, ())
+
+    def set_cont_carrier(self, active):
         if active:
-            sendVendorSetup(self.handle, SET_CONT_CARRIER, 1, 0, ())
+            _send_vendor_setup(self.handle, SET_CONT_CARRIER, 1, 0, ())
         else:
-            sendVendorSetup(self.handle, SET_CONT_CARRIER, 0, 0, ())
+            _send_vendor_setup(self.handle, SET_CONT_CARRIER, 0, 0, ())
 
-    def hasFwScann(self):
-        return self.version>=0.5
+    def _has_fw_scan(self):
+        #return self.version >= 0.5
+        # FIXME: Mitigation for Crazyradio firmware bug #9
+        return False
 
-    def scannChannels(self, start, stop, packet):
-    
-        if self.hasFwScann():# Fast firmware-driven scann
-            sendVendorSetup(self.handle, SCANN_CHANNELS, start, stop, packet)
-            return tuple(getVendorSetup(self.handle, SCANN_CHANNELS, 0, 0, 64))
-        else: # Slow PC-driven scann
+    def scan_channels(self, start, stop, packet):
+        if self._has_fw_scan():  # Fast firmware-driven scann
+            _send_vendor_setup(self.handle, SCANN_CHANNELS, start, stop,
+                               packet)
+            return tuple(_get_vendor_setup(self.handle, SCANN_CHANNELS,
+                                           0, 0, 64))
+        else:  # Slow PC-driven scann
             result = tuple()
-            for i in range(start, stop+1):
-                self.setChannel(i)
-                status = self.sendPacket(packet)
+            for i in range(start, stop + 1):
+                self.set_channel(i)
+                status = self.send_packet(packet)
                 if status and status.ack:
                     result = result + (i,)
             return result
 
     ### Data transferts ###
-    def sendPacket(self, dataOut):
+    def send_packet(self, dataOut):
         """ Send a packet and receive the ack from the radio dongle
             The ack contains information about the packet transmition
             and a data payload if the ack packet contained any """
         ackIn = None
         data = None
         try:
-            if (pyusb1 == False):
+            if (pyusb1 is False):
                 self.handle.bulkWrite(1, dataOut, 1000)
-                data = self.handle.bulkRead(0x81, 64,1000)
+                data = self.handle.bulkRead(0x81, 64, 1000)
             else:
-                self.handle.write(1, dataOut, 0, 1000);
+                self.handle.write(1, dataOut, 0, 1000)
                 data = self.handle.read(0x81, 64, 0, 1000)
-        except usb.USBError, error:
+        except usb.USBError:
             pass
 
-        if data != None:
-            ackIn = radioAck()
+        if data is not None:
+            ackIn = _radio_ack()
             if data[0] != 0:
-                ackIn.ack = (data[0]&0x01) != 0
-                ackIn.powerDet = (data[0]&0x02) != 0
-                ackIn.retry = data[0]>>4
+                ackIn.ack = (data[0] & 0x01) != 0
+                ackIn.powerDet = (data[0] & 0x02) != 0
+                ackIn.retry = data[0] >> 4
                 ackIn.data = data[1:]
             else:
                 ackIn.retry = self.arc
@@ -237,16 +257,20 @@ class Crazyradio:
 
 
 #Private utility functions
-def sendVendorSetup(handle, request, value, index, data):
+def _send_vendor_setup(handle, request, value, index, data):
     if pyusb1:
-        handle.ctrl_transfer(usb.TYPE_VENDOR, request, wValue=value, wIndex=index, timeout=1000, data_or_wLength = data)
+        handle.ctrl_transfer(usb.TYPE_VENDOR, request, wValue=value,
+                             wIndex=index, timeout=1000, data_or_wLength=data)
     else:
-        handle.controlMsg(usb.TYPE_VENDOR, request, data, value=value, index=index, timeout=1000)
+        handle.controlMsg(usb.TYPE_VENDOR, request, data, value=value,
+                          index=index, timeout=1000)
 
-def getVendorSetup(handle, request, value, index, length):
+
+def _get_vendor_setup(handle, request, value, index, length):
     if pyusb1:
-        return handle.ctrl_transfer(usb.TYPE_VENDOR | 0x80, request, wValue=value,
-                                    wIndex=index, timeout=1000, data_or_wLength = length)
+        return handle.ctrl_transfer(usb.TYPE_VENDOR | 0x80, request,
+                                    wValue=value, wIndex=index, timeout=1000,
+                                    data_or_wLength=length)
     else:
-        return handle.controlMsg(usb.TYPE_VENDOR | 0x80, request, length, value=value,
-                                index=index, timeout=1000)
+        return handle.controlMsg(usb.TYPE_VENDOR | 0x80, request, length,
+                                 value=value, index=index, timeout=1000)
